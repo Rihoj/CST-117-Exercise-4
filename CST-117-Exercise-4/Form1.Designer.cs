1ea42571e0fc302537485593a8c7f272b58da1d0
Rihoj <rihoj1@gmail.com>
2018-05-06 17:21:20 -0400

    Init commit

---
+++ CST-117-Exercise-4/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CST_117_Exercise_4
+{
+    partial class SecondsConversionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.secondsTextBox = new System.Windows.Forms.TextBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seconds conversion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seconds: ";
+            // 
+            // secondsTextBox
+            // 
+            this.secondsTextBox.Location = new System.Drawing.Point(80, 76);
+            this.secondsTextBox.Name = "secondsTextBox";
+            this.secondsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.secondsTextBox.TabIndex = 2;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(21, 134);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(231, 72);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // SecondsConversionForm
+            // 
+            this.AcceptButton = this.calculateButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 276);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.secondsTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SecondsConversionForm";
+            this.Text = "Seconds Conversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox secondsTextBox;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ CST-117-Exercise-4/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_117_Exercise_4
+{
+    public partial class SecondsConversionForm : Form
+    {
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+        int days = 0;
+
+        public SecondsConversionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            seconds = 0;
+            minutes = 0;
+            hours = 0;
+            days = 0;
+            bool result = Int32.TryParse(secondsTextBox.Text, out seconds);
+            if (result) {
+                convert();
+                writeMessage();
+            } else {
+                MessageBox.Show("Please enter an Integer for seconds.");
+            }
+        }
+        private void convert()
+        {
+            if (seconds >= 86400) {
+                calculateDays();
+            } else if (seconds >= 3600) {
+                calculateHours();
+            } else if (seconds >= 60) {
+                calculateMinutes();
+            }
+        }
+        private void calculateDays()
+        {
+            days = seconds / 86400;
+            seconds = seconds % 86400;
+            calculateHours();
+        }
+        private void calculateHours()
+        {
+            hours = seconds / 3600;
+            seconds = seconds % 3600;
+            calculateMinutes();
+        }
+        private void calculateMinutes()
+        {
+            minutes = seconds / 60;
+            seconds = seconds % 60;
+        }
+
+        private void writeMessage()
+        {
+            resultLabel.Text = "Seconds: "+seconds+"\n" +
+                "Minutes: "+minutes+"\n"+
+                "Hours: "+hours+"\n"+
+                "Days: "+days;
+        }
+    }
+}
